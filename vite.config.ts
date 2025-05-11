2427218164f060a2da75cf49d1955892e9ed03c4
McCall Bliss <missmccallkay@gmail.com>
2025-05-11 10:50:10 -0500

    add custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/substance-seeker-whisper/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
